8d2a26fb3d66deb6603f836a206fab644ed44c9a
Veronika <mainyvar@gmail.com>
2015-12-10 14:55:02 +0500

    add gear

---
+++ TestEquatMvc/Scripts/Code/js.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function(){
+    alert('hello');
+    var gear = document.getElementById("gear");
+    window.addEventListener('scroll', function () {
+        gear.style.transform = "rotate("+window.pageYOffset*0.5+"deg)";
+    });
+});
